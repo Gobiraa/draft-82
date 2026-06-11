884e29f49eeab358c87ee63cc68006498d7c5fa5
João Paulo Gobira <jpgobira2@gmail.com>
2026-06-11 15:55:00 -0300

    add: criação do banco de dados

---
+++ src/main/java/com/draft82/api/Draft82Application.java
@@ -0,0 +1,12 @@
+package com.draft82.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Draft82Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Draft82Application.class, args);
+    }
+}
